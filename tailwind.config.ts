29b4c54b8d63b02416e0e8b9c8076d08ce74e2c6
Subhadeep Sengupta <www.subhadeepsengupta2002@gmail.com>
2025-04-03 18:18:28 +0530

    config file cleared

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import type { Config } from "tailwindcss";
-import svgToDataUri from "mini-svg-data-uri";
-import * as flattenColorPalette from "tailwindcss/lib/util/flattenColorPalette";
+// import svgToDataUri from "mini-svg-data-uri";
+
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -22,32 +22,5 @@ export default {
       },
     },
   },
-  plugins: [
-    addVariablesForColors,
-    function ({ matchUtilities, theme }: any) {
-      matchUtilities(
-        {
-          "bg-dot-thick": (value: any) => ({
-            backgroundImage: `url("${svgToDataUri(
-              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="2.5"></circle></svg>`
-            )}")`,
-          }),
-        },
-        {
-          values: flattenColorPalette.default(theme("backgroundColor")),
-          type: "color",
-        }
-      );
-    },
-  ],
+  plugins: [],
 } satisfies Config;
-function addVariablesForColors({ addBase, theme }: any) {
-  const allColors = flattenColorPalette.default(theme("colors"));
-  const newVars = Object.fromEntries(
-    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
-  );
-
-  addBase({
-    ":root": newVars,
-  });
-}
