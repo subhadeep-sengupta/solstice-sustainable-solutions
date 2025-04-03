658aa78ff0c33a972a699781cdbfa9e485ed2b86
Subhadeep Sengupta <www.subhadeepsengupta2002@gmail.com>
2025-04-03 18:16:12 +0530

    lib removed

--- src/lib/utils.ts
+++
@@ -1,8 +0,0 @@
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
-import svgToDataUri from "mini-svg-data-uri";
-import { default as flattenColorPalette } from "tailwindcss/lib/util/flattenColorPalette";
